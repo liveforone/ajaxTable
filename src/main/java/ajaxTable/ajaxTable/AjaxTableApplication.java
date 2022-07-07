60b9bcdcd610ea2f830d655dde8725ddb149c9d7
liveforone <yc4852@gmail.com>
2022-07-07 18:29:46 +0900

    Initial commit

---
+++ src/main/java/ajaxTable/ajaxTable/AjaxTableApplication.java
@@ -0,0 +1,13 @@
+package ajaxTable.ajaxTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajaxTable/ajaxTable/AjaxTableApplicationTests.java
@@ -0,0 +1,13 @@
+package ajaxTable.ajaxTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
